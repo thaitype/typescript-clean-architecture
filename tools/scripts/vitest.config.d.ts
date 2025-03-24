af45894147b82fd6f31ace1b42b92b47b1c3cabb
Thada Wangthammang <thada.wth@gmail.com>
2025-03-24 22:59:55 +0700

    feat: rename tp @acme/mono tool

--- tools/scripts/vitest.config.d.ts
+++
@@ -1,3 +0,0 @@
-import { baseConfig } from "@acme/vitest-config/base";
-export default baseConfig;
-//# sourceMappingURL=vitest.config.d.ts.map
--- tools/scripts/vitest.config.js
+++
@@ -1,2 +0,0 @@
-import { baseConfig } from "@acme/vitest-config/base";
-export default baseConfig;
