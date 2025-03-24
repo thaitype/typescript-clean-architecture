851201878278af88141c031b6129efb431f7c97a
Thada Wangthammang <thada.wth@gmail.com>
2025-03-25 00:19:33 +0700

    feat: add vitest configuration and update CLI command handling

--- tools/mono/scripts/index.ts
+++ tools/mono/scripts/index.ts
@@ -1,14 +1,16 @@
 import { execa } from 'execa';
 
-let commands: string[] = [];
+let commands: string[][] = [];
 
-const commandMap: Record<string, string> = {
+const commandMap: Record<string, string | string[]> = {
   "lint:check": "eslint src",
   "lint:fix": "eslint src --fix",
   "test": "vitest run",
   "test:watch": "vitest watch",
-  "format:check": "prettier -c src",
-  "format:fix": "prettier --write src",
+  // "format:check": "prettier -c src",
+  // "format:fix": "prettier --write src",
+  "format:check": ["prettier -c ./**/*.{ts,tsx}", "prettier -c ./*.ts"],
+  "format:fix": ["prettier --write ./**/*.{ts,tsx}", "prettier --write ./*.ts"],
   "build": "esbuild ./src/index.ts --bundle --minify --platform=node --outfile=dist/index.js",
   "dev": "tsx watch ./src/index.ts",
   "start": "tsx ./src/index.ts",
@@ -18,7 +20,11 @@ const commandMap: Record<string, string> = {
 const arg = process.argv[2];
 if (arg) {
   if (commandMap[arg]) {
-    commands = (commandMap[arg].split(' '));
+    if (typeof commandMap[arg] === 'string') {
+      commands.push(commandMap[arg].split(' '));
+    } else {
+      commands = commandMap[arg].map((command) => command.split(' '));
+    }
   } else {
     console.log(`Command ${arg} not found`);
     process.exit(1);
@@ -28,7 +34,7 @@ if (arg) {
   process.exit(1);
 }
 
-(async () => {
+export async function runCommand(commands: string[]) {
   const subprocess = execa({ env: { FORCE_COLOR: 'true' }, stdout: 'pipe' })`${[...commands]}`;
   subprocess.stdout.pipe(process.stdout);
   subprocess.stderr.pipe(process.stderr);
@@ -36,4 +42,17 @@ if (arg) {
     console.error(error);
     process.exit(1);
   });
+}
+
+(async () => {
+  for(const command of commands) {
+    await runCommand(command);
+  }
+  // const subprocess = execa({ env: { FORCE_COLOR: 'true' }, stdout: 'pipe' })`${[...commands]}`;
+  // subprocess.stdout.pipe(process.stdout);
+  // subprocess.stderr.pipe(process.stderr);
+  // await subprocess.catch((error) => {
+  //   console.error(error);
+  //   process.exit(1);
+  // });
 })();
