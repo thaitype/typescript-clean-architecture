2ef42a00f52cbc3ad71a5256a4d2648eb5f16953
Thada Wangthammang <thada.wth@gmail.com>
2025-03-25 00:50:42 +0700

    refactor: simplify command execution and add utility functions for argument processing

--- tools/mono/scripts/index.ts
+++ tools/mono/scripts/index.ts
@@ -1,9 +1,7 @@
-import { execa } from 'execa';
-
-let commands: string[][] = [];
+import { type MonoScripts, processArgs, runCommand } from './libs.js';
 
 // prettier-ignore
-const commandMap: Record<string, string | string[]> = {
+const scripts: MonoScripts = {
   'lint:check': 'eslint src',
   'lint:fix': 'eslint src --fix',
   'test': 'vitest run',
@@ -14,42 +12,10 @@ const commandMap: Record<string, string | string[]> = {
   'check-types': 'tsc --noEmit',
 };
 
-const arg = process.argv[2];
-if (arg) {
-  if (commandMap[arg]) {
-    if (typeof commandMap[arg] === 'string') {
-      commands.push(commandMap[arg].split(' '));
-    } else {
-      commands = commandMap[arg].map(command => command.split(' '));
-    }
-  } else {
-    console.log(`Command ${arg} not found`);
-    process.exit(1);
-  }
-} else {
-  console.log(`Please provide a command`);
-  process.exit(1);
-}
-
-export async function runCommand(commands: string[]) {
-  const subprocess = execa({ env: { FORCE_COLOR: 'true' }, stdout: 'pipe' })`${[...commands]}`;
-  subprocess.stdout.pipe(process.stdout);
-  subprocess.stderr.pipe(process.stderr);
-  await subprocess.catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
-}
-
 (async () => {
-  for (const command of commands) {
-    await runCommand(command);
+  for (const command of processArgs(process.argv[2], scripts)) {
+    await runCommand(command, {
+      preferLocal: true,
+    });
   }
-  // const subprocess = execa({ env: { FORCE_COLOR: 'true' }, stdout: 'pipe' })`${[...commands]}`;
-  // subprocess.stdout.pipe(process.stdout);
-  // subprocess.stderr.pipe(process.stderr);
-  // await subprocess.catch((error) => {
-  //   console.error(error);
-  //   process.exit(1);
-  // });
 })();
