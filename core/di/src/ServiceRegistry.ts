6882ddeea715dc83536769a906538719900c72cb
Thada Wangthammang <thada.wth@gmail.com>
2025-03-25 09:50:05 +0700

    format: code

--- core/di/src/ServiceRegistry.ts
+++ core/di/src/ServiceRegistry.ts
@@ -2,10 +2,8 @@ import { createServiceRegistry } from '@thaitype/ioctopus';
 import { IUserRepository, UserUseCase } from '@acme/application';
 import { UserController } from '@acme/interface-adapters';
 
+// prettier-ignore
 export const registry = createServiceRegistry()
-  .define('IUserRepository')
-  .mapTo<IUserRepository>()
-  .define('UserUseCase')
-  .mapTo<UserUseCase>()
-  .define('UserController')
-  .mapTo<UserController>();
+  .define('IUserRepository').mapTo<IUserRepository>()
+  .define('UserUseCase').mapTo<UserUseCase>()
+  .define('UserController').mapTo<UserController>();
